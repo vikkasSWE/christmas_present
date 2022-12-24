0d2b5099abd1cba828fbaf10e2b89e576a0c48ef
Viktor Kasimir <kasimir.viktor@gmail.com>
2022-12-24 10:17:10 +0100

    main page

--- js/present.js
+++ js/present.js
@@ -1,5 +1,20 @@
-const present = document.getElementById("present");
+const present_alex = document.getElementById("present_alex");
+const present_oliver = document.getElementById("present_oliver");
+const present_hampus = document.getElementById("present_hampus");
+const present_wilma = document.getElementById("present_wilma");
 
-present.onclick = function (e) {
-    window.alert("hello")
+present_alex.onclick = function (e) {
+    window.alert("hello alex")
+}
+
+present_oliver.onclick = function (e) {
+    window.alert("hello oliver")
+}
+
+present_hampus.onclick = function (e) {
+    window.alert("hello hampus")
+}
+
+present_wilma.onclick = function (e) {
+    window.alert("hello wilma")
 }
